c058348ee0b8a05556f58779801cef866a9b465a
Aiza Zhenishbekova <aizazhenishbekova@MacBook-Air-Aiza.local>
2025-05-21 23:55:43 +0200

    Add full hello world SwiftUI Xcode project

---
+++ hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello world
+//
+//  Created by Aiza Zhenishbekova on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
